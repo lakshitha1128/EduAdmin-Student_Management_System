f6b5c029a5c1a8998ac8b4ae32012f38502a6551
WICRAMASINGHA W.L.C. <lakshithaw900@gmail.com>
2025-06-28 18:54:22 +0530

    upload files for EduAdmin

---
+++ EduAdmin_backend/src/main/java/com/EduAdmin/StudentManagement/EduAdminBackend.java
@@ -0,0 +1,11 @@
+package com.EduAdmin.StudentManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduAdminBackend {
+    public static void main(String[] args) {
+        SpringApplication.run(EduAdminBackend.class, args);
+    }
+}
---
+++ EduAdmin_backend/src/test/java/com/EduAdmin/StudentManagement/EduAdminBackend.java
@@ -0,0 +1,13 @@
+package com.EduAdmin.StudentManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduAdminBackend {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
